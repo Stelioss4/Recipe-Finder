78cf9ac5f6695cb2e61f2405cb3ac5d1289948b7
Stelioss4 <stelios.boursanidis@gmail.com>
2024-04-09 14:21:52 +0200

    Create a users class

---
+++ UsersProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Finder
+{
+    public class UsersProfile
+    {
+        public class UsersProfil
+        {
+            private string _name;
+
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            private string _email;
+
+            public string Email
+            {
+                get { return _email; }
+                set { _email = value; }
+            }
+
+            private string _password;
+
+            public string Password
+            {
+                get { return _password; }
+                set { _password = value; }
+            }
+        }
+    }
+}
