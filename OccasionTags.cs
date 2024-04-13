3dacfaa5895348cb486002660cd2d85e98b1822d
Stelioss4 <stelios.boursanidis@gmail.com>
2024-04-13 09:54:39 +0200

    Add a recipe class

---
+++ OccasionTags.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Finder
+{
+    public enum OccasionTags
+    {
+        breakfast,
+        brunch,
+        lunch,
+        dinner,
+        other
+    }
+}
--- Recipe.cs
+++ Recipe.cs
@@ -2,6 +2,47 @@
 {
     internal class Recipe
     {
-        
+		private string _cookingInstructions;
+
+		public string CookingInstructions
+		{
+			get { return _cookingInstructions; }
+			set { _cookingInstructions = value; }
+		}
+
+		private TimeSpan _cookingTime;
+
+		public TimeSpan CookingTime
+		{
+			get { return _cookingTime; }
+			set { _cookingTime = value; }
+		}
+
+		private string _cuisineType;
+
+		public string CuisineType
+		{
+			get { return _cuisineType; }
+			set { _cuisineType = value; }
+		}
+
+		private OccasionTags _occasionTags;
+
+		public OccasionTags OccasionTags
+		{
+			get { return _occasionTags; }
+			set { _occasionTags = value; }
+		}
+
+		private int _difficultyLevel;
+
+		public int DifficultyLevel
+        {
+			get { return _difficultyLevel; }
+			set { _difficultyLevel = value; }
+		}
+
+        public List<Ingredient> ListofIngredients { get; set; }
+
     }
 }
