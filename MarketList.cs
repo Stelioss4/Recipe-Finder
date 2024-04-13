55a59a16499f2a066c3038a6dc733a910f3b74d8
Stelioss4 <stelios.boursanidis@gmail.com>
2024-04-13 11:54:07 +0200

    add a list of links for markets

---
+++ MarketList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Finder
+{
+    public class MarketList
+    {
+		private List<string> _marketlinkList;
+
+		public List<string> MarketlinkList
+        {
+			get { return _marketlinkList; }
+			set { _marketlinkList = value; }
+		}
+
+	}
+}
