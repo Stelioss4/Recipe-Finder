d76ce2436a7fd26af70af54725846e95dff65783
Stelioss4 <stelios.boursanidis@gmail.com>
2024-04-14 09:27:32 +0200

    Add a Drink Pairing class

---
+++ DrinkPairings.cs
@@ -0,0 +1,14 @@
+﻿namespace Recipe_Finder
+{
+    public class DrinkPairings
+    {
+		private List<string> _linksForDrinkPairing;
+
+		public List<string> LinksForDrinkPairing
+        {
+			get { return _linksForDrinkPairing; }
+			set { _linksForDrinkPairing = value; }
+		}
+
+	}
+}
