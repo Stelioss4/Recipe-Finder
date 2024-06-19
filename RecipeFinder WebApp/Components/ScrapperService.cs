d40ebc2346b78ea691e39dc789384f6e70ccd715
Stelios Boursanidis <stelios.boursanidis@gmail.com>
2024-06-19 13:06:40 +0200

    Add another method which scraping another website.

--- RecipeFinder WebApp/Components/ScrapperService.cs
+++ RecipeFinder WebApp/Components/ScrapperService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RecipeFinder_WebApp.Components
 {
@@ -32,6 +33,53 @@ namespace RecipeFinder_WebApp.Components
                     foreach (var node in resultNodes)
                     {
                         var titleNode = node.SelectSingleNode(".//h3[@class='card__title']");
+                        var linkNode = node.SelectSingleNode(".//a[@class='card__titleLink']");
+                        var imageNode = node.SelectSingleNode(".//img[@class='card__img']");
+
+                        if (titleNode != null && linkNode != null)
+                        {
+                            var recipe = new Recipe
+                            {
+                                RecipeName = titleNode.InnerText.Trim(),
+                                Url = linkNode.GetAttributeValue("href", string.Empty),
+                                Image = imageNode?.GetAttributeValue("src", string.Empty)
+                            };
+                            recipes.Add(recipe);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return recipes;
+        }
+
+        public async Task<List<Recipe>> ScrappingOnChefKoch(string searchQuery)
+        {
+            List<Recipe> recipes = new List<Recipe>();
+
+            try
+            {
+                HtmlWeb web = new HtmlWeb();
+                string searchUrl = $"https://www.chefkoch.de/rs/s0/{Uri.EscapeDataString(searchQuery)}";
+                var httpClient = new HttpClient();
+                var html = await httpClient.GetStringAsync(searchUrl);
+
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(html);
+
+                // Select nodes containing recipe details               
+                var listNode = document.DocumentNode.SelectSingleNode("//*[@id=\"__layout\"]/div/div[1]/main/section");
+
+                var resultNodes = listNode.ChildNodes;
+                if (listNode != null)
+                {
+                    foreach (var node in resultNodes)
+                    {
+                        var titleNode = node.SelectSingleNode(".//h1[@class='card__title']");
                         var linkNode = node.SelectSingleNode(".//a[@class='card__titleLink']");
                         var imageNode = node.SelectSingleNode(".//img[@class='card__img']");
 
