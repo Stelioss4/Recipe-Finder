a2a14cda9c363e64f60ba86280d79f9288cd0fb7
Stelios Boursanidis <stelios.boursanidis@gmail.com>
2024-10-15 21:47:19 +0200

    Delete rapid api key from appsettings.json

--- RecipeFinder WebApp/Data/GroceryService.cs
+++ RecipeFinder WebApp/Data/GroceryService.cs
@@ -10,7 +10,7 @@ namespace RecipeFinder_WebApp.Data
         private readonly HttpClient _httpClient;
 
         public GroceryService(HttpClient httpClient)
-        {    
+        {
             _httpClient = httpClient;
         }
         public async Task<string> SearchMarketplaceForIngredient(string ingredientName)
@@ -21,17 +21,17 @@ namespace RecipeFinder_WebApp.Data
                 Method = HttpMethod.Post,
                 RequestUri = new Uri("https://edamam-food-and-grocery-database.p.rapidapi.com/api/food-database/v2/nutrients"),
                 Headers =
-        {
-            { "x-rapidapi-key", "your_api_key_here" },
-            { "x-rapidapi-host", "edamam-food-and-grocery-database.p.rapidapi.com" },
-        },
+                    {
+                        { "x-rapidapi-key", "your_api_key_here" },
+                        { "x-rapidapi-host", "edamam-food-and-grocery-database.p.rapidapi.com" },
+                    },
                 // Adjust this part to use the ingredient name and valid measure URI
                 Content = new StringContent($"{{\"ingredients\":[{{\"quantity\":1,\"measureURI\":\"your_measure_uri_here\",\"qualifiers\":[],\"foodId\":\"{ingredientName}\"}}]}}")
                 {
                     Headers =
-            {
-                ContentType = new MediaTypeHeaderValue("application/json")
-            }
+                    {
+                        ContentType = new MediaTypeHeaderValue("application/json")
+                    }
                 }
             };
 
