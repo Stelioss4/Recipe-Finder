d2b2b34bd29957740cb57c13e9b2fe837a5b0a22
Stelios Boursanidis <stelios.boursanidis@gmail.com>
2025-01-16 10:49:04 +0100

    Rename user to start with a lowercase, and delete comment out code from dataService which is actually unused

--- RecipeFinder WebApp/Data/DataService.cs
+++ RecipeFinder WebApp/Data/DataService.cs
@@ -25,186 +25,11 @@ namespace RecipeFinder_WebApp.Data
         public DataService(NavigationManager Navigation, IHttpClientFactory clientFactory, IDbContextFactory<ApplicationDbContext> contextFactory, UserManager<ApplicationUser> userManager, AuthenticationStateProvider authenticationStateProvider)
         {
             _clientFactory = clientFactory;
-            //       context = context ?? throw new ArgumentNullException(nameof(context));
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
             AuthenticationStateProvider = authenticationStateProvider;
             _navigation = Navigation;
             _contextFactory = contextFactory;
         }
-
-        ///// <summary>
-        ///// Add Ingredient from appUser's recipes to a shopping list
-        ///// </summary>
-        ///// <returns></returns>
-        //public async Task AddIngredientsToShoppingList(Ingredient ingredient)
-        //{
-        //    using var context = _contextFactory.CreateDbContext();
-
-        //    var appUser = await GetAuthenticatedUserAsync();
-
-        //    User = appUser.User;
-
-        //    if (User != null)
-        //    {
-        //        if (User.ShoppingList.Contains(ingredient))
-        //        {
-        //            Console.WriteLine("ingredient already in shopping list");
-        //        }
-        //        else
-        //        {
-        //            User.ShoppingList.Add(ingredient);
-
-        //            await context.SaveChangesAsync();
-
-        //            Console.WriteLine("Ingredient is successfully added to shopping list");
-
-        //        }
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Removes Ingredient from shopping list
-        ///// </summary>
-        ///// <param name="ingredient"></param>
-        ///// <returns></returns>
-        //public async Task RemoveIngredientFromShoppingListAsync(Ingredient ingredient)
-        //{
-        //    using var context = _contextFactory.CreateDbContext();
-
-        //    try
-        //    {
-        //        var appUser = await GetAuthenticatedUserAsync();
-
-
-        //        User = appUser.User;
-
-        //        if (User != null)
-        //        {
-        //            var ingredientToRemove = User.ShoppingList
-        //                .FirstOrDefault(i =>
-        //                 i.Id == ingredient.Id && i.UserId == ingredient.UserId);
-
-        //            if (ingredientToRemove != null)
-        //            {
-        //                // Remove the recipe from the list
-        //                User.ShoppingList.Remove(ingredientToRemove);
-
-        //                // Save changes to the database
-        //                await context.SaveChangesAsync();
-
-        //                Console.WriteLine("Ingredient removed from ShoppingList successfully.");
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine($"Error removing ingredient from Shopping list: {ex.Message}");
-        //        // Handle the error appropriately (e.g., display an error message)
-        //    }
-        //}
-
-
-        ///// <summary>
-        ///// Adds a recipe to the ClaimUser's favorites and saves the changes to the database.
-        ///// </summary>
-        //public async Task AddFavoriteRecipeAsync(Recipe recipe)
-        //{
-        //    using var context = _contextFactory.CreateDbContext();
-
-        //    var appUser = await GetAuthenticatedUserAsync();
-        //    context.Update(appUser);
-
-        //    if (appUser?.User != null)
-        //    {
-        //        // Access the appUser's favorite recipes directly due to AutoInclude
-        //        if (appUser.User.FavoriteRecipes.Any(r => r.Id == recipe.Id))
-        //        {
-        //            Console.WriteLine("Recipe is already in your favorites.");
-        //            return;
-        //        }
-
-        //        // Retrieve the tracked recipe
-        //        var trackedRecipe = await context.Recipes.FirstOrDefaultAsync(r => r.Id == recipe.Id);
-        //        if (trackedRecipe == null)
-        //        {
-        //            Console.WriteLine("Recipe not found in the database.");
-        //            return;
-        //        }
-
-        //        // Add the recipe to favorites
-        //        appUser.User.FavoriteRecipes.Add(trackedRecipe);
-        //        await context.SaveChangesAsync();
-
-        //        Console.WriteLine("Recipe added to your favorites successfully.");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("User is not authenticated.");
-        //        _navigation.NavigateTo("account/login");
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Removes a recipe from the authenticated appUser's list of favorite recipes.
-        ///// </summary>
-        //public async Task RemoveFavoriteRecipeAsync(Recipe recipe)
-        //{
-        //    using var _context = _contextFactory.CreateDbContext();
-
-        //    try
-        //    {
-        //        var appUser = await GetAuthenticatedUserAsync();
-        //        _context.Update(appUser);
-
-
-        //        if (appUser.User != null)
-        //        {
-
-        //            // Use SourceDomain and SearchTerms as additional criteria
-        //            var recipeToRemove = appUser.User.FavoriteRecipes
-        //             .FirstOrDefault(r =>
-        //                 string.Equals(r.RecipeName, recipe.RecipeName, StringComparison.OrdinalIgnoreCase) &&
-        //                 string.Equals(r.SourceDomain, recipe.SourceDomain, StringComparison.OrdinalIgnoreCase)
-
-        //             //r.SearchTerms != null &&
-        //             //recipe.SearchTerms != null &&
-        //             //r.SearchTerms
-        //             //    .OrderBy(t => t.Term) 
-        //             //    .Select(t => t.Term) 
-        //             //    .SequenceEqual(
-        //             //        recipe.SearchTerms.OrderBy(t => t.Term).Select(t => t.Term)
-        //             );
-
-
-
-
-        //            if (recipeToRemove != null)
-        //            {
-        //                // Remove the recipe from the list
-        //                appUser.User.FavoriteRecipes.Remove(recipeToRemove);
-
-        //                // Save changes to the database
-        //                await _context.SaveChangesAsync();
-
-        //                Console.WriteLine("Recipe removed from favorites successfully.");
-        //            }
-        //            else
-        //            {
-        //                Console.WriteLine("Recipe not found in favorites.");
-        //            }
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("User is not authenticated.");
-        //            _navigation.NavigateTo("account/login");
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine($"Error removing recipe from favorites: {ex.Message}");
-        //        // Handle the error appropriately (e.g., display an error message)
-        //    }
-        //}
 
         /// <summary>
         /// Retrieves saved recipes from database based on search terms and source.
@@ -334,6 +159,8 @@ namespace RecipeFinder_WebApp.Data
                     existingRecipe.Ratings.Add(newRating);
                     existingRecipe.Reviews.Add(newReview);
 
+                    newRating.Profile = appUser.User;
+                    newReview.Profile = appUser.User;
 
                     await _context.SaveChangesAsync();
                 }
