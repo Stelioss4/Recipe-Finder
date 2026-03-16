516c0d4ea2938c3bcf86bab862c4f094c18d7e2f
Stelios Boursanidis <stelios.boursanidis@gmail.com>
2026-03-16 11:29:33 +0100

    Add boolean fields for dietary user preferences

---
+++ Recipe Finder/UserPreferences.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Finder
+{
+    public class UserPreferences
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public bool IsVegetarian { get; set; }
+        public bool IsVegan { get; set; }
+        public bool IsGlutenFree { get; set; }
+        public bool IsDairyFree { get; set; }
+        public bool IsLowCarb { get; set; }
+    }
+}
